96c69321b85b6d054d999c53d8af4c87cbbe432c
WDB40 <wbrown1640@gmail.com>
2019-01-21 20:43:30 -0600

    Adding the RunAllTests class.

---
+++ src/test/java/RunAllTests.java
@@ -0,0 +1,19 @@
+import controller.LibraryTest;
+import model.BookTest;
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.junit.runner.notification.Failure;
+
+public class RunAllTests {
+
+    public static void main(String[] args) {
+        Result result = JUnitCore.runClasses(LibraryTest.class, BookTest.class);
+
+        for(Failure failure : result.getFailures()){
+            System.out.println(failure.toString());
+        }
+
+        System.out.println(result.wasSuccessful());
+    }
+
+}
